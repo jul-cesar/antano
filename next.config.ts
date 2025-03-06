34829d5cd18d5e0bcb8db52e89fb48154787aa01
Julio Cesar <juliocesarmartinez1344@gmail.com>
2025-03-06 15:41:49 -0500

    eslint config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */typescript: {
+  typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
