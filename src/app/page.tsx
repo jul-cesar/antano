012d37117cb5da31047abaf86d4298ced958cce1
Julio Cesar <juliocesarmartinez1344@gmail.com>
2025-03-10 16:13:13 -0500

    link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
 import DateTimePickerPage from "@/components/DateTimePicker";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <section className="container">
+      <Link href={"/dashboard"}>
+        <Button variant={"link"}>Ver lista de reservas hechas</Button>
+      </Link>
       <DateTimePickerPage />
     </section>
   );
